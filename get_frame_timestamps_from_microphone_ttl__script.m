f7eab1cf5a75e538ba234c02ed6cd1e2d1e2f11d
Alessandro La Chioma <ale.lach@gmail.com>
2023-08-10 12:01:05 -0400

    Update get_frame_timestamps_from_microphone_ttl__script.m

--- get_frame_timestamps_from_microphone_ttl__script.m
+++ get_frame_timestamps_from_microphone_ttl__script.m
@@ -3,7 +3,7 @@
 
 clear
 
-folder_root = 'Y:\Users\pernik\20230807\m1\trial1_rubber';
+folder_root = 'Y:\Users\pernik\20230807\m1\trial5_rubber';
 
 ls_mic = dir([folder_root filesep '*_mic.wav']);
 if length(ls_mic) == 1
@@ -57,6 +57,8 @@ hold on;
 plot(ttl)
 plot(locs,pks, 'v');
 xlabel('Audio samples')
+% xlim([0 Fs*2]) % first 2 seconds
+% xlim([length(ttl)-[Fs*2, 0]]) % last 2 seconds
 
 figure
 histogram(diff(locs)/Fs*1000)
@@ -67,21 +69,29 @@ xlabel('Frame time (ms)')
 path_vid     = path_mic;
 filename_vid = [filename_datetime_tag '_cam1.avi'];
 
+fprintf('Loading the video file to get the nr. of video frames, this will take a minute for long videos...\n');
 v = VideoReader(fullfile(path_vid,filename_vid));
+fprintf('Video file loaded, nr. of video frames: %d \n', v.NumFrames);
+
 v.NumFrames;
 
 if v.NumFrames ~= length(locs)
     fprintf('\n ! ! ! ! ! \n');
     fprintf(' The number of video frames is different from the number of TTL onsets extracted ! \n');
+    fprintf(' The TTL onset extraction could have missed some TTLs, or some frames could have been dropped.\n')
+    fprintf(' N.B. dropped frames do have a TTL (=camera sensor has been exposed) but they are not saved in the video file.\n')
     fprintf(' ! ! ! ! ! \n\n');
 end
 
+
+k = 0.05;
 d_locs_sec = diff(locs)/Fs;
 frametime_snd = mean(d_locs_sec);
-nr_dropped_frames = length( find(d_locs_sec > frametime_snd*1.5) );
-if nr_dropped_frames
+nr_problematic_ttls = length( find(d_locs_sec > frametime_snd*(1+k) | d_locs_sec < frametime_snd*(1-k)) );
+if nr_problematic_ttls
     fprintf('\n ! ! ! ! ! \n');
-    fprintf(' Based on inter-frame interval, %d frames were dropped ! \n', nr_dropped_frames);
+    fprintf(' Based on inter-TTL interval, %d TTLs were too close or too far apart compared to expected! \n', nr_problematic_ttls);
+    fprintf(' Likely something is wrong with the TTL onset extraction... \n')
     fprintf(' ! ! ! ! ! \n\n');
 end
 
@@ -97,13 +107,38 @@ filename_vidcsv = [filename_datetime_tag '_cam1.csv'];
 T = readmatrix(fullfile(path_vidcsv,filename_vidcsv), ...
         'OutputType','double', 'Delimiter',',');
 
-df = (T(end,3)+1) - v.NumFrames;
-if df
+%% Check for dropped frames
+
+% nr_dropped_frames_id = (T(end,3)+1) - v.NumFrames;
+nr_dropped_frames_id = sum( diff(T(:,3)) > 1 );
+if nr_dropped_frames_id
     fprintf('\n ! ! ! ! ! \n');
-    fprintf(' Based on the frame IDs, %d frames were dropped ! \n', df);
+    fprintf(' Based on the frame IDs, %d frames were dropped ! \n', nr_dropped_frames_id);
     fprintf(' ! ! ! ! ! \n\n');
 end
+inds_dropped_id = find(diff(T(:,3)) > 1);
+dt_dropped_frames_id = (T(inds_dropped_id+1,1)-T(inds_dropped_id,1))/1e9;
 
+dt_cam_sec = diff(T(:,1)/1e9);
+frametime_cam = mean(dt_cam_sec);
+nr_dropped_frames_cam = length( find(dt_cam_sec > frametime_cam*1.5) );
+if nr_dropped_frames_cam
+    fprintf('\n ! ! ! ! ! \n');
+    fprintf(' Based on inter-frame interval of camera timestamps, %d frames were dropped ! \n', nr_dropped_frames_cam);
+    fprintf(' ! ! ! ! ! \n\n');
+end
+inds_dropped = find(dt_cam_sec > frametime_cam*1.5);
+dt_dropped_frames_cam = (T(inds_dropped+1,1)-T(inds_dropped,1))/1e9;
+
+
+dt_bon_sec = diff(T(:,2));
+frametime_bon = mean(dt_bon_sec);
+nr_dropped_frames_bon = length( find(dt_bon_sec > frametime_bon*1.9) );
+% if nr_dropped_frames_bon
+%     fprintf('\n ! ! ! ! ! \n');
+%     fprintf(' Based on inter-frame interval of bonsai timestamps, %d frames were dropped ! \n', nr_dropped_frames_bon);
+%     fprintf(' ! ! ! ! ! \n\n');
+% end
 
 %% Get duration, frame time, and frame rate according to all clocks
 
